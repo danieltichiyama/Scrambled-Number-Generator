0a8bc7323c3cc15ce424c67dd492163511002721
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-09-28 09:41:34 -1000

    finishes challenge, ready for review

--- generator.js
+++ generator.js
@@ -1,3 +1,15 @@
 module.exports = function(n) {
-  // do work here
+  let array = [];
+
+  for (i = 0; i < n; i++) {
+    array.push(i);
+  }
+
+  array.map(function(e) {
+    array.push(array[Math.floor(Math.random() * n * 10)]);
+  });
+
+  array = array.slice(n);
+
+  return array;
 };
